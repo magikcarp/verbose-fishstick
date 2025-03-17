b789cf171fb0d69f59cbffae87f75f9a7226a8b2
magikcarp <pike.amc@gmail.com>
2025-03-17 16:05:03 -0400

    Removed redundant transformation classes

--- unet/dataset.py
+++ unet/dataset.py
@@ -5,11 +5,27 @@ from torchvision import transforms
 import numpy as np
 import cv2
 
+import matplotlib.pyplot as plt
+
 
 class ImageSample:
-    def __init__(self, image, mask):
+    """ Gray-scale image and mask with dimensions [C x W x H] where C is the
+        number of color channels, W is the width of the image, and H is the
+        height of the image. 
+    """
+    def __init__(self, image: torch.Tensor, mask: torch.Tensor):
         self.image = image
         self.mask = mask
+
+    def display(self):
+        plt.figure(figsize=(16, 8))
+        plt.subplot(121)
+        plt.title("Image")
+        plt.imshow(self.image.transpose(0, 2))
+        plt.subplot(122)
+        plt.title("Mask")
+        plt.imshow(self.mask.transpose(0, 2))
+        pass
 
 
 class FluoCellsDataset(torch.utils.data.Dataset):
@@ -17,37 +33,43 @@ class FluoCellsDataset(torch.utils.data.Dataset):
         self.root = root
         self.transforms = transforms
 
-        self.imgs = list(sorted(os.listdir(os.path.join(root, "images"))))
+        self.images = list(sorted(os.listdir(os.path.join(root, "images"))))
         self.masks = list(sorted(os.listdir(os.path.join(root, "masks"))))
 
     def __getitem__(self, index: int) -> ImageSample:
-        img_path = os.path.join(self.root, "images", self.imgs[index])
-        mask_path = os.path.join(self.root, "images", self.masks[index])
+        img_path = os.path.join(self.root, "images", self.images[index])
+        msk_path = os.path.join(self.root, "masks", self.masks[index])
 
-        img = cv2.imread(img_path)
-        msk = cv2.cvtColor(cv2.imread(mask_path), cv2.COLOR_BGR2GRAY)
-        sample = {"image": img, "mask": msk}
+        img = torch.from_numpy(
+            np.expand_dims(
+                cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_RGB2GRAY),
+                axis=0
+            )
+        ).float().transpose(1, 2) / 255
+        msk = torch.from_numpy(
+            np.expand_dims(
+                cv2.cvtColor(cv2.imread(msk_path), cv2.COLOR_RGB2GRAY),
+                axis=0
+            )
+        ).float().transpose(1, 2) / 255
+        
+        
+        smpl = ImageSample(img, msk)
 
         if self.transforms:
-            sample = self.transforms(sample)
+            smpl = self.transforms(smpl)
 
-        return sample
+        return smpl
+
+    def __len__(self):
+        return len(self.images)
 
 
-class Resize:
+class Tile:
     def __init__(self, out_size: int):
         self.out_size = out_size
 
-    def __call__(self, sample: ImageSample) -> ImageSample:
-        img_resized = cv2.resize(sample.image, (self.out_size, self.out_size))
-        msk_resized = torch.from_numpy(sample.image).float().transpose(0, 2) / 255
-        return ImageSample(img_resized, msk_resized)
-
-
-class ToTensor:
     def __call__(self, sample: ImageSample):
-        img_tensor = torch.from_numpy(sample.image).float().transpose(0, 2) / 255
-        msk_tensor = torch.from_numpy(
-            np.expand_dims(sample.mask, axis=0)
-        ).float().transpose(1, 2)
-        return ImageSample(img_tensor, msk_tensor)
+        pass
+        
+
