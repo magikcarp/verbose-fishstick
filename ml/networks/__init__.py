051e0dac8bce115d87b710b7f2cccef56a503fd1
magikcarp <pike.amc@gmail.com>
2025-03-29 09:31:54 -0400

    Updated UNet docs

---
+++ ml/networks/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from .unet import UNet, seed_unet_kernels
+from .wnet import WNet
--- ml/networks/wnet.py
+++ ml/networks/wnet.py
@@ -1,12 +1,46 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """ WNet constructed from UNet. """
 
 import torch
+import torch.nn as nn
 
-from .unet import UNet, MiniUNet
+from .unet import UNet
 
 
-class WNet(torch.nn.Module):
-    def __init__(self):
+class WNet(nn.Module):
+    """
+    W-Net architecture for unsupervised image segmentation. 
+
+    Inspired by the description as outlined in https://arxiv.org/abs/1711.08506.
+
+    Attributes:
+        enc_unet (UNet): UNet arm that encodes images
+        dec_unet (UNet): UNet arm that decodes image representation
+
+    The number of kernels can be customized if desired. Example network sizes:
+    - UNet : [64, 128, 256, 512, 1024]
+    - SmallUNet : [32, 64, 128, 256, 512]
+    - MiniUNet : [16, 32, 64, 128, 256]
+    - TinyUNet : [8, 16, 32, 64, 128]
+    - DeepUNet : [8, 16, 32, 64, 128, 256, 512, 1024]
+        - Should only be used with very large images due to down sampling
+        (image demnsions are halved after each layer of double convolution)
+    """
+    def __init__(self, 
+                 in_channels: int, 
+                 out_channels: int, 
+                 unet_shape: list[int]):
         super().__init__()
+        self.enc_unet = UNet(in_channels, out_channels, unet_shape)
+        self.dec_unet = UNet(out_channels, in_channels, unet_shape)
+
+        def forward_enc():
+            pass
+
+        def forward_dec():
+            pass
+
+        def forward():
+            pass
