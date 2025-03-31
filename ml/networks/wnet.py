8b4219a8c8c8043b5fc25c53cfab269e0e39e4d5
magikcarp <pike.amc@gmail.com>
2025-03-31 12:53:43 -0400

    Added method to train W-Net

--- ml/networks/wnet.py
+++ ml/networks/wnet.py
@@ -5,9 +5,10 @@
 
 import torch
 import torch.nn as nn
+from torch.nn.functional import mse_loss
 
 from .unet import UNet
-
+from losses import NCutLoss
 
 class WNet(nn.Module):
     """
@@ -36,11 +37,51 @@ class WNet(nn.Module):
         self.enc_unet = UNet(in_channels, out_channels, unet_shape)
         self.dec_unet = UNet(out_channels, in_channels, unet_shape)
 
-        def forward_enc():
-            pass
+    def forward_enc(self, x: torch.Tensor) -> torch.Tensor:
+        segmentation = self.enc_unet(x)
+        return segmentation
 
-        def forward_dec():
-            pass
+    def forward_dec(self, x: torch.Tensor) -> torch.Tensor:
+        reconstruction = self.dec_unet(x)
+        return reconstruction
 
-        def forward():
-            pass
+    def forward(self, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+        segmentation = self.enc_unet(x)
+        reconstruction = self.dec_unet(segmentation)
+        return segmentation, reconstruction
+    
+    def train_model(
+            self,
+            train_loader: torch.utils.data.DataLoader,
+            n_epochs: int,
+            lr: float=0.0005,
+            verbose: bool=False,
+            report_iter: int=25,
+            ) -> None:
+        self.train()
+
+        optim_enc = torch.optim.Adam(self.enc_unet.parameters(), lr=lr)
+        optim_all = torch.optim.Adam(self.parameters(), lr=lr)
+
+        ncl = NCutLoss()
+        
+        for e in range(n_epochs):
+            if verbose:
+                print(f"Epoch {e+1}")
+            for i, batch in enumerate(train_loader):
+                # soft cut N loss pass
+                optim_enc.zero_grad()
+                seg = self.enc_unet(batch)
+                soft_cut_loss = ncl(seg, batch)
+                soft_cut_loss.backward(retain_graph=False)
+                optim_enc.step()
+
+                # complete forward pass
+                optim_all.zero_grad()
+                seg, recon = self(batch)
+                recon_loss = mse_loss(seg, recon, reduction="sum")
+                recon_loss.backward(retain_graph=False)
+                optim_all.step()
+
+                if verbose and i % report_iter == report_iter - 1:
+                    print(f" - batch {i+1} loss: {recon_loss}")
